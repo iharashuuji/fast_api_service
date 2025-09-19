8ef92904ddf9ef3db7aa41b62954af9ab6222cb5
iharashuuji <5011hiroto@gmail.com>
2025-09-19 10:43:50 +0900

    git add .!

--- backend/tests/test_todo_service.py
+++ backend/tests/test_todo_service.py
@@ -1,5 +1,5 @@
-from backend.app.services.todo_service import TodoService
-from backend.app.schemas.todo import TodoCreate
+from app.services.todo_service import TodoService
+from app.schemas.todo import TodoCreate
 import sys
 from pathlib import Path
 
